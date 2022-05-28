b8cfba269b393393a7832e4019ecce2ce8781716
Matusevich_Aliaksei <Tag92_92@mail.ru>
2022-05-28 12:19:52 +0300

    task1

--- PYTHON/task1.py
+++ PYTHON/task1.py
@@ -1,8 +1,8 @@
 print("HELLo world!")
 rub = 5
 kop = 22
-tovar = 5
-tovar_5 = (rub * 100 + kop) * tovar 
-rub_5 = tovar_5 // 100
-kop_5 = tovar_5 % 100
+goods = 5
+goodsx5 = (rub * 100 + kop) * goods 
+rub_5 = goodsx5 // 100
+kop_5 = goodsx5 % 100
 print("Сумма пяти товаров -", rub_5, 'руб', kop_5, 'коп')
