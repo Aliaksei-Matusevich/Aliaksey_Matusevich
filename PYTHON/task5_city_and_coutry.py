c7c02d06d605d8161f8671d98eba19df5f1b6e12
Matusevich_Aliaksei <Tag92_92@mail.ru>
2022-06-23 17:38:16 +0300

    complete

--- PYTHON/task5_city_and_coutry.py
+++ PYTHON/task5_city_and_coutry.py
@@ -1,5 +1,5 @@
-n = int(input())
 cities = dict()
+n = int(input())
 for i in range(n):
     country = input().split()
     for city in country[1:]:
@@ -10,3 +10,16 @@ for i in range(m):
     city = input()
     result.append(cities[city])    
 print(*result, sep='\n')
+
+
+"""
+4
+Belarus Minsk Grodno Vitebsk Gomel Brest
+Russia Moscow Saint-Petersburg Smolensk
+USA New_York Detroit Los_Angeles
+Poland Krakow Warsaw Gdansk Belostok
+3
+Saint-Petersburg
+New_York
+Los_Angeles
+"""
